80db7ea11b9f9e463b5f79bb4f9c80edf184d1cc
ngari-kev <kevngariwangui@gmail.com>
2024-04-09 15:33:52 +0300

    betty corrections

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
-	
+
 	depth = 0;
 
 	if (tree == NULL)
@@ -16,8 +16,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree->parent != NULL)
 	{
 		depth = 1 + binary_tree_depth(tree->parent);
-		
 	}
-	
+
 	return (depth);
 }
